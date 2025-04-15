4e1d12175f28f1d5be944e7f6dfdad9fd28f2369
johanneBiO <jobao@dtu.dk>
2025-04-15 09:46:22 +0200

    Remove overall code folder.

--- processing/prepare_dataset.py
+++ processing/prepare_dataset.py
@@ -207,34 +207,37 @@ def extract_accessions_from_fasta(input_path, output_path):
 # Generate datasets
 #---------------------------------------------------------------------------------------#
 
+# Initialize the random number generator
+random.seed(10)
+
 # Read adjusted sequence lengths
-csv_df = pd.read_csv("../../data/complete/additional/seq_lengths_adj.csv")
+csv_df = pd.read_csv("../data/complete/additional/seq_lengths_adj.csv")
 
 # Create a dictionary with accession as the key and adjusted length as the value
 length_dict = dict(zip(csv_df['accession'], csv_df['length_adj']))
 
 # Specify the file name of the fasta and resulting cropped file
-bio_fasta = Path("../../data/complete/sequences/biological_seq/UP000005640_9606_sp.fasta") 
-bio_cropped_fasta = Path("../../data/complete/sequences/biological_seq/UP000005640_9606_sp_cropped.fasta")
+bio_fasta = Path("../data/complete/sequences/biological_seq/UP000005640_9606_sp.fasta") 
+bio_cropped_fasta = Path("../data/complete/sequences/biological_seq/UP000005640_9606_sp_cropped.fasta")
 
 # Crop sequences
 crop_fasta(bio_fasta, bio_cropped_fasta, length_dict, esm_max = 1024)
 
 # Specify the filename of subsets
-subset_01000_cropped_fasta = Path("../../data/subset_01000/sequences/biological_seq/seq_01000.fasta")
-subset_00100_cropped_fasta = Path("../../data/subset_00100/sequences/biological_seq/seq_00100.fasta")
+subset_01000_cropped_fasta = Path("../data/subset_01000/sequences/biological_seq/seq_01000.fasta")
+subset_00100_cropped_fasta = Path("../data/subset_00100/sequences/biological_seq/seq_00100.fasta")
 
 # Sample random sequences
 extract_random_sample(bio_cropped_fasta, subset_01000_cropped_fasta, num_sequences=1000)
 extract_random_sample(subset_01000_cropped_fasta, subset_00100_cropped_fasta, num_sequences=100)
 
 # Extract the accesions for the sequences across subsets.
-extract_accessions_from_fasta(subset_01000_cropped_fasta, Path("../../data/subset_01000/additional/accessions.txt"))
-extract_accessions_from_fasta(subset_00100_cropped_fasta, Path("../../data/subset_00100/additional/accessions.txt"))
+extract_accessions_from_fasta(subset_01000_cropped_fasta, Path("../data/subset_01000/additional/accessions.txt"))
+extract_accessions_from_fasta(subset_00100_cropped_fasta, Path("../data/subset_00100/additional/accessions.txt"))
 
 # Specify directories for scrambled sequences (- only for subsets)
-subset_01000_scramble_seq_dir = Path("../../data/subset_01000/sequences/scrambled_seq/")
-subset_00100_scramble_seq_dir = Path("../../data/subset_00100/sequences/scrambled_seq/")
+subset_01000_scramble_seq_dir = Path("../data/subset_01000/sequences/scrambled_seq/")
+subset_00100_scramble_seq_dir = Path("../data/subset_00100/sequences/scrambled_seq/")
 
 # Scramble sequences
 for i in range(1, 6):
