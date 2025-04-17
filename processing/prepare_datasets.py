bc18fe30ba6c8d9a6a884e9223258bc022fb44d7
johanneBiO <jobao@dtu.dk>
2025-04-17 17:12:06 +0200

    Make dir for bio and scr sequence.

--- processing/prepare_datasets.py
+++ processing/prepare_datasets.py
@@ -27,23 +27,27 @@ bio_cropped_fasta = "../data/complete/sequences/seq_complete_sp_cropped.fasta"
 crop_fasta(bio_fasta, bio_cropped_fasta, length_dict, esm_max = 1024)
 
 # Extract the accesion for cropped sequences
-extract_accessions_from_fasta(bio_cropped_fasta, Path("../data/complete/additional/accessions_sp_cropped.txt"))
+extract_accessions_from_fasta(bio_cropped_fasta, "../data/complete/additional/accessions_sp_cropped.txt")
 
 # Specify the filename of subsets
-subset_01000_cropped_fasta = Path("../data/subset_01000/sequences/biological_seq/seq_01000.fasta")
-subset_00100_cropped_fasta = Path("../data/subset_00100/sequences/biological_seq/seq_00100.fasta")
+os.makedirs("../data/subset_01000/sequences/biological_seq", exist_ok=True)
+os.makedirs("../data/subset_00100/sequences/biological_seq", exist_ok=True)
+subset_01000_cropped_fasta = "../data/subset_01000/sequences/biological_seq/seq_01000.fasta"
+subset_00100_cropped_fasta = "../data/subset_00100/sequences/biological_seq/seq_00100.fasta"
 
 # Sample random sequences
 extract_random_sample(bio_cropped_fasta, subset_01000_cropped_fasta, num_sequences=1000)
 extract_random_sample(subset_01000_cropped_fasta, subset_00100_cropped_fasta, num_sequences=100)
 
 # Extract the accesions for the sequences across subsets.
-extract_accessions_from_fasta(subset_01000_cropped_fasta, Path("../data/subset_01000/additional/accessions.txt"))
-extract_accessions_from_fasta(subset_00100_cropped_fasta, Path("../data/subset_00100/additional/accessions.txt"))
+extract_accessions_from_fasta(subset_01000_cropped_fasta, "../data/subset_01000/additional/accessions.txt")
+extract_accessions_from_fasta(subset_00100_cropped_fasta, "../data/subset_00100/additional/accessions.txt")
 
 # Specify directories for scrambled sequences (- only for subsets)
-subset_01000_scramble_seq_dir = Path("../data/subset_01000/sequences/scrambled_seq/")
-subset_00100_scramble_seq_dir = Path("../data/subset_00100/sequences/scrambled_seq/")
+subset_01000_scramble_seq_dir = "../data/subset_01000/sequences/scrambled_seq/"
+subset_00100_scramble_seq_dir = "../data/subset_00100/sequences/scrambled_seq/"
+os.makedirs(subset_01000_scramble_seq_dir, exist_ok=True)
+os.makedirs(subset_00100_scramble_seq_dir, exist_ok=True)
 
 # Scramble sequences
 for i in range(1, 6):
