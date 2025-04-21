02c5fc16374ac6b584c7cb039a2c8f1ae05074fb
johanneBiO <jobao@dtu.dk>
2025-04-21 16:22:33 +0200

    New script for analyzing the annotations. Adding common theme.

---
+++ analysis/utils/theme.R
@@ -0,0 +1,8 @@
+main_theme <- theme_classic() +
+  theme(axis.title.x = element_text(face = "bold", vjust = -1),
+        axis.title.y = element_text(face = "bold", vjust = 3),
+        legend.title = element_text(face = "bold", size = 11),
+        legend.text = element_text(size = 11))
+
+
+#c("#990000", "#799bb9", "#1f618d")
